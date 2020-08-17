90912aa2b7b4f424e3e033d0a475496c36a15e9c
Caroline Marques <carolscope@localhost.localdomain>
2020-08-17 13:05:57 -0300

    First commit

---
+++ history/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Nav class="mb-6" />
+    <HelloWorld msg="Welcome to Your Vue.js App" class="text-center"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Nav from './components/Nav.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Nav
+  }
+}
+</script>
+
+<style>
+</style>
